f2f59e31ac4476bb25e90f2c575c215bc44e5391
stanislavsynyasvskyy <153124369+sasykk@users.noreply.github.com>
2024-04-03 22:29:12 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='helper',
+      version='0.0.1',
+      description='Very usefull code',
+      url='https://github.com/sasykk/homework1_web.git',
+      author='Alliance',
+      author_email='',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+            'markdown',
+      ],
+      entry_points={'console_scripts': ['run=src.main:run']},
+      include_package_data=True,
+      zip_safe=False)
